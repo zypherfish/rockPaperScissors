2772b5118e89f469f4ceb3ce13265fa45d88ae04
zypherfish <248241723+zypherfish@users.noreply.github.com>
2025-12-14 12:36:14 +0530

    rock paper scissors!

--- script.js
+++ script.js
@@ -4,15 +4,77 @@ function getComputerChoice(){
     // get a rock paper or scissors base on random number
     let result = "unknown"
     if (randomNumber <= 0.3 && randomNumber >= 0){
-        result = "Rock"
+        result = "rock";
     }else if (randomNumber >= 0.4 && randomNumber <= 0.6){
-        result = "Scissors"
+        result = "scissors";
     }else{
-        result = "Paper"
+        result = "paper";
     }
     // return the result
-    return result
+    return result;
 }
 
+let ComputerScore = 0;
+let humanScore = 0;
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let userChoice = prompt("choice : rock , paper , scissors ?");
+    userChoice = userChoice.toLowerCase();
+    return userChoice;
+}
+
+function showWinner(winner, losser, winloss = 'win'){
+    console.log(`You ${winloss}! ${winner} beats ${losser}`)
+}
+//scores
+
+function playRound(humanChoice, computerChoice){
+    // if tie stop no changes
+    if(humanChoice == computerChoice){
+        console.log(`Tie! your choice: ${humanChoice} and computer : ${computerChoice}`)
+        humanScore++;
+        ComputerScore++;
+    }
+    // if human = rock win if computer = scissor else loss
+    else if (humanChoice == 'rock'){
+        if (computerChoice == 'scissors'){
+            showWinner(humanChoice, computerChoice)
+            humanScore++;
+        }else {showWinner(computerChoice, humanChoice, 'lose')
+            ComputerScore++;
+        }
+    }
+    // if human = paper win id comp = rock else loss
+    else if (humanChoice == 'paper'){
+        if (computerChoice == 'rock'){
+            showWinner(humanChoice, computerChoice)
+            humanScore++;
+        }else{
+            showWinner(computerChoice, humanChoice, 'lose')
+            ComputerScore++;
+        }
+    }
+    // if human = scissors win if comp = paper else loss
+    else if (humanChoice == 'scissors'){
+        if (computerChoice == 'paper'){
+            showWinner(humanChoice, computerChoice)
+            humanScore++;
+        }else {(showWinner(humanChoice, computerChoice, 'lose'))
+            ComputerScore++;
+        }
+    }
+}
+
+function playGame(){
+    let round = 1;
+    humanScore = 0;
+    ComputerScore = 0;
+    while(round <= 5){
+        console.log(`round: ${round}`)
+        playRound(getHumanChoice(),getComputerChoice());
+        console.log(humanScore,ComputerScore)
+        round++;
+    }
+}
+
+playGame();
